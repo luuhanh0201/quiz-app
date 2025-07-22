0a325d930cbf4f885b6abf72f0b71cd46faa3f74
Luu Hanh <luuhanh0201@gmail.com>
2025-07-23 06:14:33 +0700

    feat(quizzes): get all quizzes by id user

--- src/assets/images/index.jsx
+++
@@ -1,8 +0,0 @@
-import noAvatar from "@/assets/images/avatar-error.png";
-import noImage from "@/assets/images/no-img.jpg";
-const images = {
-    noAvatar,
-    noImage,
-
-};
-export default images;
--- src/pages/ProfilePage/MyProfile/index.jsx
+++ src/pages/ProfilePage/MyProfile/index.jsx
@@ -5,9 +5,52 @@ import images from "@/assets/images";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuestion } from "@fortawesome/free-solid-svg-icons";
 import { Link, NavLink, useOutletContext } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useAuth } from "@/contexts/authContext";
 const cx = classNames.bind(styles);
 function MyProfile() {
+    const HOST = import.meta.env.VITE_HOST
     const profile = useOutletContext()
+    const [quizzes, setQuizzes] = useState([])
+    const { token } = useAuth()
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_GET_QUIZZES, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then(res => {
+                setQuizzes(res.data.data)
+            })
+            .catch(err => {
+                console.log("Lỗi khi gọi get quizzes:", err.response);
+            });
+    }, [])
+    console.log(quizzes)
+    const ItemQuizzes = quizzes.map((item) => {
+        console.log(item)
+        return (
+            <div key={item.id} className={cx("quiz-item")}>
+                <Image className={cx("image-item")} src={
+                    item?.coverImage
+                        ? item.coverImage.startsWith("http")
+                            ? item.coverImage
+                            : `${HOST}${item.coverImage}`
+                        : images.noAvatar
+                } />
+                <div className={cx("content-item")}>
+                    <span className={cx("name-quiz")}>{item.title}</span>
+                    <p>
+                        <span className={cx("quantity-quiz")}>
+                            <FontAwesomeIcon icon={faQuestion} /> 22
+                        </span>
+                        <span className={cx("date-create-quizzes")}>45/6</span>
+                    </p>
+                </div>
+            </div>
+        )
+    })
     return (
         <div>
             <div className={cx("container-profile")}>
@@ -26,7 +69,9 @@ function MyProfile() {
                     <div className={cx("button-create-quiz")}>
                         <Link to={"/create-quizzes"} className={cx("button")}>Create Quiz</Link>
                     </div>
-                    <div className={cx("quiz-item")}>
+
+                    {ItemQuizzes}
+                    {/* <div className={cx("quiz-item")}>
                         <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
                         <div className={cx("content-item")}>
                             <span className={cx("name-quiz")}>hello, this is name quiz</span>
@@ -37,127 +82,8 @@ function MyProfile() {
                                 <span className={cx("date-create-quizzes")}>45/6</span>
                             </p>
                         </div>
-                    </div>{" "}
-                    <div className={cx("quiz-item")}>
-                        <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
-                        <div className={cx("content-item")}>
-                            <span className={cx("name-quiz")}>hello, this is name quiz</span>
-                            <p>
-                                <span className={cx("quantity-quiz")}>
-                                    <FontAwesomeIcon icon={faQuestion} /> 22
-                                </span>
-                                <span className={cx("date-create-quizzes")}>45/6</span>
-                            </p>
-                        </div>
-                    </div>{" "}
-                    <div className={cx("quiz-item")}>
-                        <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
-                        <div className={cx("content-item")}>
-                            <span className={cx("name-quiz")}>hello, this is name quiz</span>
-                            <p>
-                                <span className={cx("quantity-quiz")}>
-                                    <FontAwesomeIcon icon={faQuestion} /> 22
-                                </span>
-                                <span className={cx("date-create-quizzes")}>45/6</span>
-                            </p>
-                        </div>
-                    </div>{" "}
-                    <div className={cx("quiz-item")}>
-                        <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
-                        <div className={cx("content-item")}>
-                            <span className={cx("name-quiz")}>hello, this is name quiz</span>
-                            <p>
-                                <span className={cx("quantity-quiz")}>
-                                    <FontAwesomeIcon icon={faQuestion} /> 22
-                                </span>
-                                <span className={cx("date-create-quizzes")}>45/6</span>
-                            </p>
-                        </div>
-                    </div>{" "}
-                    <div className={cx("quiz-item")}>
-                        <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
-                        <div className={cx("content-item")}>
-                            <span className={cx("name-quiz")}>hello, this is name quiz</span>
-                            <p>
-                                <span className={cx("quantity-quiz")}>
-                                    <FontAwesomeIcon icon={faQuestion} /> 22
-                                </span>
-                                <span className={cx("date-create-quizzes")}>45/6</span>
-                            </p>
-                        </div>
-                    </div>{" "}
-                    <div className={cx("quiz-item")}>
-                        <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
-                        <div className={cx("content-item")}>
-                            <span className={cx("name-quiz")}>hello, this is name quiz</span>
-                            <p>
-                                <span className={cx("quantity-quiz")}>
-                                    <FontAwesomeIcon icon={faQuestion} /> 22
-                                </span>
-                                <span className={cx("date-create-quizzes")}>45/6</span>
-                            </p>
-                        </div>
-                    </div>{" "}
-                    <div className={cx("quiz-item")}>
-                        <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
-                        <div className={cx("content-item")}>
-                            <span className={cx("name-quiz")}>hello, this is name quiz</span>
-                            <p>
-                                <span className={cx("quantity-quiz")}>
-                                    <FontAwesomeIcon icon={faQuestion} /> 22
-                                </span>
-                                <span className={cx("date-create-quizzes")}>45/6</span>
-                            </p>
-                        </div>
-                    </div>{" "}
-                    <div className={cx("quiz-item")}>
-                        <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
-                        <div className={cx("content-item")}>
-                            <span className={cx("name-quiz")}>hello, this is name quiz</span>
-                            <p>
-                                <span className={cx("quantity-quiz")}>
-                                    <FontAwesomeIcon icon={faQuestion} /> 22
-                                </span>
-                                <span className={cx("date-create-quizzes")}>45/6</span>
-                            </p>
-                        </div>
-                    </div>{" "}
-                    <div className={cx("quiz-item")}>
-                        <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
-                        <div className={cx("content-item")}>
-                            <span className={cx("name-quiz")}>hello, this is name quiz</span>
-                            <p>
-                                <span className={cx("quantity-quiz")}>
-                                    <FontAwesomeIcon icon={faQuestion} /> 22
-                                </span>
-                                <span className={cx("date-create-quizzes")}>45/6</span>
-                            </p>
-                        </div>
-                    </div>{" "}
-                    <div className={cx("quiz-item")}>
-                        <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
-                        <div className={cx("content-item")}>
-                            <span className={cx("name-quiz")}>hello, this is name quiz</span>
-                            <p>
-                                <span className={cx("quantity-quiz")}>
-                                    <FontAwesomeIcon icon={faQuestion} /> 22
-                                </span>
-                                <span className={cx("date-create-quizzes")}>45/6</span>
-                            </p>
-                        </div>
-                    </div>{" "}
-                    <div className={cx("quiz-item")}>
-                        <Image className={cx("image-item")} src={profile.avatar || images.noImage} />
-                        <div className={cx("content-item")}>
-                            <span className={cx("name-quiz")}>hello, this is name quiz</span>
-                            <p>
-                                <span className={cx("quantity-quiz")}>
-                                    <FontAwesomeIcon icon={faQuestion} /> 22
-                                </span>
-                                <span className={cx("date-create-quizzes")}>45/6</span>
-                            </p>
-                        </div>
-                    </div>
+                    </div> */}
+
                 </div>
             </div>
         </div>
