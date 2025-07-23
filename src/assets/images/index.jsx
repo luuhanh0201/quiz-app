e3c5a3602dbb1e6e95e7441e5cb8371c63d8c69a
Luu Hanh <luuhanh0201@gmail.com>
2025-07-24 00:10:37 +0700

    Restore deleted image and component files

---
+++ src/assets/images/index.jsx
@@ -0,0 +1,8 @@
+import noAvatar from "@/assets/images/avatar-error.png";
+import noImage from "@/assets/images/no-img.jpg";
+const images = {
+    noAvatar,
+    noImage,
+
+};
+export default images;
